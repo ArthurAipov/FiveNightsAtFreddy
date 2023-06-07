ba1169ca4146ac06ac21195b77b545130219c7d9
Arthur Aipov <aipov2@gmail.com>
2023-06-07 18:04:38 +0300

    Askar commit

--- FiveNightsAtFreddy/Models/CameraCameraScreen.cs
+++ FiveNightsAtFreddy/Models/CameraCameraScreen.cs
@@ -1,16 +1,39 @@
 ﻿using FiveNightsAtFreddy.DataBase;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FiveNightsAtFreddy.Models
 {
-    public class CameraCameraScreen
+    public class CameraCameraScreen    
     {
         public int Id { get; set; }
-        public Camera CameraForListView { get; set; }   
-        public CameraScreen ScreenCamera { get; set; }
+        public Camera CameraForListView { get; set; }
+        
+        CameraScreen screenCamera;
+        public CameraScreen ScreenCamera 
+        {
+            get
+            {
+                return screenCamera;
+            }
+
+            set
+            {
+                screenCamera = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        public void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
--- FiveNightsAtFreddy/Pages/CamerasPage.xaml.cs
+++ FiveNightsAtFreddy/Pages/CamerasPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using FiveNightsAtFreddy.Models;
+﻿using FiveNightsAtFreddy.DataBase;
+using FiveNightsAtFreddy.Models;
 using FiveNightsAtFreddy.Windows;
 using System;
 using System.Collections.Generic;
@@ -13,7 +14,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
@@ -25,61 +25,37 @@ namespace FiveNightsAtFreddy.Pages
     /// </summary>
     public partial class CamerasPage : Page
     {
-        public ObservableCollection<CameraCameraScreen> CameraList = new ObservableCollection<CameraCameraScreen>();
-        public Random random = new Random();
+        List<CameraCameraScreen> CameraList = new List<CameraCameraScreen>();
+        Random random = new Random();
+        Dictionary<DispatcherTimer, Camera> dispatchers = new Dictionary<DispatcherTimer, Camera>();
+
         public CamerasPage()
         {
             InitializeComponent();
-            var timerForFirstCamera = new DispatcherTimer();
-            var timerForSecondCamera = new DispatcherTimer();
-            var timerForThirdCamera = new DispatcherTimer();
-            timerForFirstCamera.Tick += TimerForFirstCamera_Tick;
-            timerForSecondCamera.Tick += TimerForSecondCamera_Tick;
-            timerForThirdCamera.Tick += TimerForThirdCamera_Tick;
-            timerForFirstCamera.Interval = new TimeSpan(0, 0, GlobalSettings.DB.Camera.ToList()[0].Frequency);
-            timerForSecondCamera.Interval = new TimeSpan(0, 0, GlobalSettings.DB.Camera.ToList()[1].Frequency);
-            timerForThirdCamera.Interval = new TimeSpan(0, 0, GlobalSettings.DB.Camera.ToList()[2].Frequency);
-            timerForFirstCamera.Start();
-            timerForThirdCamera.Start();
-            timerForSecondCamera.Start();
-            Refresh();
-
-        }
-
-        private void TimerForFirstCamera_Tick(object sender, EventArgs e)
-        {
-            var screenForFirstCamera = GlobalSettings.ScreensForFirst[random.Next(0, GlobalSettings.ScreensForFirst.Count - 1)];
-            CameraList[0] = new CameraCameraScreen { Id = 2, ScreenCamera = screenForFirstCamera, CameraForListView = GlobalSettings.DB.Camera.ToList()[1] };
-            Refresh();
-        }
-
-        private void TimerForSecondCamera_Tick(object sender, EventArgs e)
-        {
-            var screenForSecondCamera = GlobalSettings.ScreensForSecond[random.Next(0, GlobalSettings.ScreensForSecond.Count - 1)];
-            CameraList[0] = new CameraCameraScreen { Id = 2, ScreenCamera = screenForSecondCamera, CameraForListView = GlobalSettings.DB.Camera.ToList()[1] };
-            Refresh();
-        }
-
-        private void TimerForThirdCamera_Tick(object sender, EventArgs e)
-        {
-            var screenForThirdCamera = GlobalSettings.ScreensForThird[random.Next(0, GlobalSettings.ScreensForThird.Count - 1)];
-            CameraList[0] = new CameraCameraScreen { Id = 3, ScreenCamera = screenForThirdCamera, CameraForListView = GlobalSettings.DB.Camera.ToList()[2] };
-            Refresh();
-        }
-
-        public void Refresh()
-        {
-            if (CameraList.Count == 0)
+            CameraList = new List<CameraCameraScreen>();
+            foreach (var camera in GlobalSettings.DB.Camera)
             {
-                var screenForFirstCamera = GlobalSettings.ScreensForFirst[random.Next(0, GlobalSettings.ScreensForFirst.Count - 1)];
-                var screenForSecondCamera = GlobalSettings.ScreensForSecond[random.Next(0, GlobalSettings.ScreensForSecond.Count - 1)];
-                var screenForThirdCamera = GlobalSettings.ScreensForThird[random.Next(0, GlobalSettings.ScreensForThird.Count - 1)];
-                CameraList.Add(new CameraCameraScreen { Id = 1, ScreenCamera = screenForFirstCamera, CameraForListView = GlobalSettings.DB.Camera.ToList()[0] });
-                CameraList.Add(new CameraCameraScreen { Id = 2, ScreenCamera = screenForSecondCamera, CameraForListView = GlobalSettings.DB.Camera.ToList()[1] });
-                CameraList.Add(new CameraCameraScreen { Id = 3, ScreenCamera = screenForThirdCamera, CameraForListView = GlobalSettings.DB.Camera.ToList()[2] });
-
+                CameraList.Add(new CameraCameraScreen() { CameraForListView = camera, ScreenCamera = camera.CameraScreen.FirstOrDefault() });
+                InitializeCameraObserve(camera);
             }
             ListViewCameras.ItemsSource = CameraList;
+        }
+
+        private void InitializeCameraObserve(Camera camera)
+        {
+            var dispatcherTimer = new DispatcherTimer();
+            dispatchers.Add(dispatcherTimer, camera);
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(camera.Frequency);
+            dispatcherTimer.Tick += CameraObserve_Tick;
+            dispatcherTimer.Start();
+        }
+
+        private void CameraObserve_Tick(object sender, EventArgs e)
+        {
+            var dispatcherTimer = sender as DispatcherTimer;
+            var camera = dispatchers[dispatcherTimer];
+            var screen = camera.CameraScreen.ToList()[random.Next(0, camera.CameraScreen.Count)];
+            CameraList.FirstOrDefault(f => f.CameraForListView == camera).ScreenCamera = screen;
         }
 
         private void ButtonBack_Click(object sender, RoutedEventArgs e)
